cde63987bbb6e7d0e1f43ea77b2a5c0e97403ce6
James Darling <jdarling@credera.com>
2020-01-11 09:01:57 -0600

    Added database project. This inlcdes the table that we will use in EF in the other apps.

---
+++ Database/Madlibs.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Madlibs](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[StudentName] [varchar](500) NULL,
+	[FavoriteFood] [varchar](200) NULL,
+	[FavoriteNumber] [int] NULL,
+	[FavoriteColor] [varchar](10) NULL,
+	[FavoriteAnimal] [varchar](500) NULL
+)
